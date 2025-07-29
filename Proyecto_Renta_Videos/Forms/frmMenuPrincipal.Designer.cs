8a3bda71182e4eab2e76e0eb89d6182cd1a83293
EStevensCambranesM <ecambranesm@miumg.edu.gt>
2025-07-29 15:34:32 -0600

    Creando Formularios

---
+++ Proyecto_Renta_Videos/Forms/frmMenuPrincipal.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Proyecto_Renta_Videos.Forms
+{
+    partial class frmMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msNavegacion = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // msNavegacion
+            // 
+            this.msNavegacion.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msNavegacion.Location = new System.Drawing.Point(0, 0);
+            this.msNavegacion.Name = "msNavegacion";
+            this.msNavegacion.Size = new System.Drawing.Size(900, 24);
+            this.msNavegacion.TabIndex = 0;
+            this.msNavegacion.Text = "menuStrip1";
+            // 
+            // frmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.msNavegacion);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.msNavegacion;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMenuPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msNavegacion;
+    }
+}
