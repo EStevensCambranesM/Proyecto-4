589bb386667d0c134c54a504bde512692d832a96
Juanka20-Sandoval00 <jsandovalq@miumg.edu.gt>
2025-08-02 13:04:27 -0600

    Modificacion de formularios de Reportes

--- Proyecto_Renta_Videos/Forms/frmGananciasYPerdidas.cs
+++ Proyecto_Renta_Videos/Forms/frmGananciasYPerdidas.cs
@@ -16,5 +16,65 @@ namespace Proyecto_Renta_Videos.Forms
         {
             InitializeComponent();
         }
+
+        private void lblGanancias_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmGananciasYPerdidas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtTotaldeRentas.Text, out double rentas) &&
+        double.TryParse(txtOtrosIngresos.Text, out double otros))
+            {
+                double totalIngresos = rentas + otros;
+                txtTotaldeIngresos.Text = totalIngresos.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("Ingrese valores válidos para rentas y otros ingresos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCalcularPerdida_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtGastosOperativos.Text, out double gastos) &&
+        double.TryParse(txtMantenimiento.Text, out double mantenimiento))
+            {
+                double totalPerdidas = gastos + mantenimiento;
+                txtTotaldePerdidas.Text = totalPerdidas.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("Ingrese valores válidos para gastos y mantenimiento.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void lblMantenimiento_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPerdidasTotales_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVolverMenu_Click(object sender, EventArgs e)
+        {
+            frmMenuPrincipal menu = new frmMenuPrincipal();
+            menu.Show();
+            this.Close();
+        }
     }
 }
--- Proyecto_Renta_Videos/Forms/frmRentasPorCliente.Designer.cs
+++ Proyecto_Renta_Videos/Forms/frmRentasPorCliente.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.lblRentaPorCliente = new System.Windows.Forms.Label();
+            this.lblIDCliente = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblIDProducto = new System.Windows.Forms.Label();
+            this.lblNombreProducto = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtIDCliente = new System.Windows.Forms.TextBox();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtIDProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
             this.dgvRenta = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -37,8 +53,6 @@
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRenta)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,15 +60,157 @@
             // 
             this.lblRentaPorCliente.AutoSize = true;
             this.lblRentaPorCliente.Font = new System.Drawing.Font("Arial Narrow", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRentaPorCliente.Location = new System.Drawing.Point(492, 11);
+            this.lblRentaPorCliente.Location = new System.Drawing.Point(355, 9);
             this.lblRentaPorCliente.Name = "lblRentaPorCliente";
-            this.lblRentaPorCliente.Size = new System.Drawing.Size(198, 31);
+            this.lblRentaPorCliente.Size = new System.Drawing.Size(247, 40);
             this.lblRentaPorCliente.TabIndex = 0;
             this.lblRentaPorCliente.Text = "Renta Por Cliente";
             this.lblRentaPorCliente.Click += new System.EventHandler(this.lblRentaPorCliente_Click);
             // 
+            // lblIDCliente
+            // 
+            this.lblIDCliente.AutoSize = true;
+            this.lblIDCliente.Location = new System.Drawing.Point(12, 102);
+            this.lblIDCliente.Name = "lblIDCliente";
+            this.lblIDCliente.Size = new System.Drawing.Size(97, 25);
+            this.lblIDCliente.TabIndex = 1;
+            this.lblIDCliente.Text = "ID Cliente";
+            this.lblIDCliente.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(12, 163);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(81, 25);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(12, 237);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(89, 25);
+            this.lblTelefono.TabIndex = 3;
+            this.lblTelefono.Text = "Teléfono";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(12, 316);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(114, 25);
+            this.lblDescripcion.TabIndex = 4;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // lblIDProducto
+            // 
+            this.lblIDProducto.AutoSize = true;
+            this.lblIDProducto.Location = new System.Drawing.Point(466, 102);
+            this.lblIDProducto.Name = "lblIDProducto";
+            this.lblIDProducto.Size = new System.Drawing.Size(114, 25);
+            this.lblIDProducto.TabIndex = 5;
+            this.lblIDProducto.Text = "ID Producto";
+            // 
+            // lblNombreProducto
+            // 
+            this.lblNombreProducto.AutoSize = true;
+            this.lblNombreProducto.Location = new System.Drawing.Point(466, 166);
+            this.lblNombreProducto.Name = "lblNombreProducto";
+            this.lblNombreProducto.Size = new System.Drawing.Size(195, 25);
+            this.lblNombreProducto.TabIndex = 6;
+            this.lblNombreProducto.Text = "Nombre del Producto";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(466, 240);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(162, 25);
+            this.lblCantidad.TabIndex = 7;
+            this.lblCantidad.Text = "Cantidad rentada";
+            // 
+            // txtIDCliente
+            // 
+            this.txtIDCliente.Location = new System.Drawing.Point(115, 102);
+            this.txtIDCliente.Multiline = true;
+            this.txtIDCliente.Name = "txtIDCliente";
+            this.txtIDCliente.Size = new System.Drawing.Size(238, 35);
+            this.txtIDCliente.TabIndex = 8;
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Location = new System.Drawing.Point(115, 163);
+            this.txtNombreCliente.Multiline = true;
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(238, 34);
+            this.txtNombreCliente.TabIndex = 9;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(115, 237);
+            this.txtTelefono.Multiline = true;
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(238, 40);
+            this.txtTelefono.TabIndex = 10;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(132, 316);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(741, 140);
+            this.txtDescripcion.TabIndex = 11;
+            // 
+            // txtIDProducto
+            // 
+            this.txtIDProducto.Location = new System.Drawing.Point(667, 99);
+            this.txtIDProducto.Multiline = true;
+            this.txtIDProducto.Name = "txtIDProducto";
+            this.txtIDProducto.Size = new System.Drawing.Size(206, 37);
+            this.txtIDProducto.TabIndex = 12;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(667, 163);
+            this.txtNombreProducto.Multiline = true;
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(206, 47);
+            this.txtNombreProducto.TabIndex = 13;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(667, 237);
+            this.txtCantidad.Multiline = true;
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(206, 40);
+            this.txtCantidad.TabIndex = 14;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(1, 9);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(140, 39);
+            this.btnVolver.TabIndex = 15;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(345, 494);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(316, 67);
+            this.btnGuardar.TabIndex = 16;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
             // dgvRenta
             // 
+            this.dgvRenta.AllowUserToAddRows = false;
+            this.dgvRenta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvRenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvRenta.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -63,87 +219,87 @@
             this.Column4,
             this.Column5,
             this.Column6,
-            this.Column7,
-            this.Column8,
-            this.Column9});
-            this.dgvRenta.Location = new System.Drawing.Point(-6, 95);
+            this.Column7});
+            this.dgvRenta.Location = new System.Drawing.Point(72, 602);
             this.dgvRenta.Name = "dgvRenta";
+            this.dgvRenta.ReadOnly = true;
             this.dgvRenta.RowHeadersWidth = 51;
             this.dgvRenta.RowTemplate.Height = 24;
-            this.dgvRenta.Size = new System.Drawing.Size(1245, 578);
-            this.dgvRenta.TabIndex = 1;
-            this.dgvRenta.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRenta_CellContentClick);
+            this.dgvRenta.Size = new System.Drawing.Size(930, 150);
+            this.dgvRenta.TabIndex = 17;
             // 
             // Column1
             // 
-            this.Column1.HeaderText = "ID_Cliente";
+            this.Column1.HeaderText = "ID Cliente";
             this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
-            this.Column1.Width = 125;
+            this.Column1.ReadOnly = true;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Nombre";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
-            this.Column2.Width = 125;
+            this.Column2.ReadOnly = true;
             // 
             // Column3
             // 
-            this.Column3.HeaderText = "Fecha_Renta";
+            this.Column3.HeaderText = "Telefono";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
-            this.Column3.Width = 125;
+            this.Column3.ReadOnly = true;
             // 
             // Column4
             // 
-            this.Column4.HeaderText = "Monto";
+            this.Column4.HeaderText = "Descripcion";
             this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
-            this.Column4.Width = 125;
+            this.Column4.ReadOnly = true;
             // 
             // Column5
             // 
-            this.Column5.HeaderText = "Nombre_Pelicula";
+            this.Column5.HeaderText = "ID Producto";
             this.Column5.MinimumWidth = 6;
             this.Column5.Name = "Column5";
-            this.Column5.Width = 125;
+            this.Column5.ReadOnly = true;
             // 
             // Column6
             // 
-            this.Column6.HeaderText = "Genero";
+            this.Column6.HeaderText = "Nombre Produc";
             this.Column6.MinimumWidth = 6;
             this.Column6.Name = "Column6";
-            this.Column6.Width = 125;
+            this.Column6.ReadOnly = true;
             // 
             // Column7
             // 
-            this.Column7.HeaderText = "Estado";
+            this.Column7.HeaderText = "Cantidad Rentada";
             this.Column7.MinimumWidth = 6;
             this.Column7.Name = "Column7";
-            this.Column7.Width = 125;
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "Metodo_Pago";
-            this.Column8.MinimumWidth = 6;
-            this.Column8.Name = "Column8";
-            this.Column8.Width = 125;
-            // 
-            // Column9
-            // 
-            this.Column9.HeaderText = "Multa";
-            this.Column9.MinimumWidth = 6;
-            this.Column9.Name = "Column9";
-            this.Column9.Width = 125;
+            this.Column7.ReadOnly = true;
             // 
             // frmRentasPorCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(1252, 723);
+            this.ClientSize = new System.Drawing.Size(1082, 1055);
             this.Controls.Add(this.dgvRenta);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtIDProducto);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.txtIDCliente);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblNombreProducto);
+            this.Controls.Add(this.lblIDProducto);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblIDCliente);
             this.Controls.Add(this.lblRentaPorCliente);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmRentasPorCliente";
@@ -159,6 +315,22 @@
         #endregion
 
         private System.Windows.Forms.Label lblRentaPorCliente;
+        private System.Windows.Forms.Label lblIDCliente;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblIDProducto;
+        private System.Windows.Forms.Label lblNombreProducto;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtIDCliente;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtIDProducto;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.DataGridView dgvRenta;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
@@ -167,7 +339,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
     }
 }
