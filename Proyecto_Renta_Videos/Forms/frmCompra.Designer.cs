9c20c75906378ccdae011b7646c0b3318c696583
Diego Saquil <dsaquilg@miumg.edu.gt>
2025-08-06 18:09:37 -0600

    CONEXIONES CON BD y formulario nueva proveedor

--- Proyecto_Renta_Videos/Forms/frmCompra.Designer.cs
+++ Proyecto_Renta_Videos/Forms/frmCompra.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCompra));
             this.txtCompra = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtPrecio = new System.Windows.Forms.Label();
@@ -38,10 +37,10 @@
             this.txtCompras = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.tlsRegreso = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.regresarAMenùToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtCompra
@@ -73,7 +72,7 @@
             // txtfecha
             // 
             this.txtfecha.AutoSize = true;
-            this.txtfecha.Location = new System.Drawing.Point(138, 267);
+            this.txtfecha.Location = new System.Drawing.Point(138, 279);
             this.txtfecha.Name = "txtfecha";
             this.txtfecha.Size = new System.Drawing.Size(45, 16);
             this.txtfecha.TabIndex = 3;
@@ -88,13 +87,12 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(415, 344);
             this.dataGridView1.TabIndex = 4;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // txtListaCompra
             // 
             this.txtListaCompra.AutoSize = true;
             this.txtListaCompra.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtListaCompra.Location = new System.Drawing.Point(705, 92);
+            this.txtListaCompra.Location = new System.Drawing.Point(687, 92);
             this.txtListaCompra.Name = "txtListaCompra";
             this.txtListaCompra.Size = new System.Drawing.Size(172, 27);
             this.txtListaCompra.TabIndex = 5;
@@ -110,7 +108,6 @@
             this.txtCompras.Size = new System.Drawing.Size(289, 37);
             this.txtCompras.TabIndex = 6;
             this.txtCompras.Text = "Datos de la compra";
-            this.txtCompras.Click += new System.EventHandler(this.label5_Click);
             // 
             // textBox2
             // 
@@ -128,25 +125,23 @@
             this.textBox3.Size = new System.Drawing.Size(154, 30);
             this.textBox3.TabIndex = 8;
             // 
-            // toolStrip1
+            // menuStrip1
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tlsRegreso});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1012, 27);
-            this.toolStrip1.TabIndex = 9;
-            this.toolStrip1.Text = "toolStrip1";
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.regresarAMenùToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1012, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // tlsRegreso
+            // regresarAMenùToolStripMenuItem
             // 
-            this.tlsRegreso.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.tlsRegreso.Image = ((System.Drawing.Image)(resources.GetObject("tlsRegreso.Image")));
-            this.tlsRegreso.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tlsRegreso.Name = "tlsRegreso";
-            this.tlsRegreso.Size = new System.Drawing.Size(124, 24);
-            this.tlsRegreso.Text = "Regreso al menú";
+            this.regresarAMenùToolStripMenuItem.Name = "regresarAMenùToolStripMenuItem";
+            this.regresarAMenùToolStripMenuItem.Size = new System.Drawing.Size(134, 24);
+            this.regresarAMenùToolStripMenuItem.Text = "Regresar a menù";
+            this.regresarAMenùToolStripMenuItem.Click += new System.EventHandler(this.regresarAMenùToolStripMenuItem_Click);
             // 
             // frmCompra
             // 
@@ -154,7 +149,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(1012, 524);
-            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtCompras);
@@ -164,11 +158,13 @@
             this.Controls.Add(this.txtPrecio);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtCompra);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmCompra";
             this.Text = "frmCompra";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,7 +181,7 @@
         private System.Windows.Forms.Label txtCompras;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton tlsRegreso;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem regresarAMenùToolStripMenuItem;
     }
 }
